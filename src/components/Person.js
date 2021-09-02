6dd9e7ebb36b9158b20458c6445913a99afa1180
Sergio Gregorutti <sergiogregorutti@gmail.com>
2021-09-02 01:36:13 -0300

    Remove console.log

--- src/components/Person.js
+++ src/components/Person.js
@@ -5,16 +5,13 @@ function Person(props) {
   const [person, setPerson] = useState("");
 
   useEffect(() => {
-    console.log('props.location.search', props.location.search);
     const { username } = queryString.parse(props.location.search);
-    console.log('username', username);
     const url = `https://api.github.com/users/${username}`;
 
     const fetchData = async () => {
         try {
             const response = await fetch(url);
             const json = await response.json();
-            console.log('json', json);
             setPerson(json);
         } catch (error) {
             console.log("error", error);
@@ -27,7 +24,7 @@ function Person(props) {
   return (
     <div className="container">
       <div className="card">
-        <img src={person.avatar_url} />
+        <img alt={person.name} src={person.avatar_url} />
         <h3>{person.name}</h3>
         <span>{person.location}</span>
       </div>
